f232fc9137585538e3149763b643d390bfbba0b1
baerongmerong <151704516+baerongmerong@users.noreply.github.com>
2023-12-01 20:36:35 +0900

    Add files via upload

---
+++ CalculatorStep1.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+class Calculator {
+    func calculate(operator: String, firstNumber: Int, secondNumber: Int) -> Double? {
+        if `operator` == "+" {
+            return Double(firstNumber + secondNumber)
+        }
+        if `operator` == "-" {
+            return Double(firstNumber - secondNumber)
+        }
+        if `operator` == "*" {
+            return Double(firstNumber * secondNumber)
+        }
+        if `operator` == "/" {
+            return Double(firstNumber / secondNumber)
+        }
+        
+    return nil
+    }
+}
+
+let calculator = Calculator()
+if let addResult = calculator.calculate(operator: "+", firstNumber: 10, secondNumber: 20) {
+    print("addResult: \(addResult)")
+}
+if let subtractResult = calculator.calculate(operator: "-", firstNumber: 30, secondNumber: 20) {
+    print("subtractResult: \(subtractResult)")
+}
+if let multiplyResult = calculator.calculate(operator: "*", firstNumber: 10, secondNumber: 5) {
+    print("multiplyResult: \(multiplyResult)")
+}
+if let divideResult = calculator.calculate(operator: "/", firstNumber: 40, secondNumber: 10) {
+    print("divideResult: \(divideResult)")
+}
